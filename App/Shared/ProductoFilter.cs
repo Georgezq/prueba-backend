667670083d81c42ab8ccd01258771983bb3b257f
Georgezq <yosho2mendoza@gmail.com>
2025-04-16 16:39:49 -0500

    Agregar clases 'BodegaFilter' y 'ProductoFilter' para soporte de paginación y filtrado

---
+++ App/Shared/ProductoFilter.cs
@@ -0,0 +1,9 @@
+public class ProductoFilter
+{
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public string? Global { get; set; }
+    public string? SortField { get; set; }
+    public int SortOrder { get; set; }
+
+}
